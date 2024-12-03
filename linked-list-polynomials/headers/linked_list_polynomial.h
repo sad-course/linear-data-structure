6c4900f0c9a4cb9d092b506dcf114d9c9675d842
Mirla Lopes <mirlaisabel11@gmail.com>
2024-12-03 07:34:51 -0300

    feat: Add child class for polynomial

--- linked-list-polynomials/headers/linked_list_polynomial.h
+++ linked-list-polynomials/headers/linked_list_polynomial.h
@@ -1,8 +1,19 @@
 //
 // Created by mirla-lopes on 03/12/24.
 //
-
+#include "linked_list.h"
 #ifndef LINKED_LIST_POLYNOMIAL_H
 #define LINKED_LIST_POLYNOMIAL_H
+
+class LinkedListPolynomial : public LinkedList {
+    private:
+        Node *head;
+
+    public:
+        LinkedListPolynomial();
+        ~LinkedListPolynomial();
+
+        void insert(int coefficient, int exponent);
+};
 
 #endif //LINKED_LIST_POLYNOMIAL_H
