dec4686c511cb685345f1c80c32194ce1f23ee7f
Mirla Lopes <mirlaisabel11@gmail.com>
2024-12-03 07:31:56 -0300

    feat: Implement node methods

--- linked-list-polynomials/src/node.cpp
+++ linked-list-polynomials/src/node.cpp
@@ -1,3 +1,40 @@
 //
 // Created by mirla-lopes on 27/11/24.
 //
+
+#include "node.h"
+
+// Node::~Node() {
+//     delete this;
+// }
+
+int Node::getFirstValue() const {
+    return this->firstValue;
+}
+
+void Node::setFirstValue(int firstValue) {
+    this->firstValue = firstValue;
+}
+
+int Node::getSecondValue() const {
+    return this->secondValue;
+}
+
+void Node::setSecondValue(int secondValue) {
+    this->secondValue = secondValue;
+}
+
+Node *Node::getNext(){
+    return this->next;
+}
+
+void Node::setNext(Node *next) {
+    this->next = next;
+}
+
+Node Node::operator+(const Node& rhs) const{
+    Node temp(0,0);
+    temp.setFirstValue(this->getFirstValue() + rhs.getFirstValue());
+    temp.setSecondValue(this->getSecondValue() + rhs.getSecondValue());
+    return temp;
+}
